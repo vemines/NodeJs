f4e25e68efa38f8129860fcbade9ace9959c268b
VeMines <vemines@outlook.com>
2024-01-25 18:55:50 +0700

    init

---
+++ RestApi/server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+require('dotenv').config();
+const app = require('./src/app')
+
+const PORT = process.env.PORT || 3003
+
+const server = app.listen(PORT, () => {
+    console.log(`WSV start with port ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+    server.close(() => console.log(`exits server express`))
+})
